7e7a9e687e015956e4a3e910b388028ce1d6c34e
Arstan <arsiksad0v.wgod@gmail.com>
2024-06-29 00:51:36 +0600

    Created component for editing posts

---
+++ src/pages/EditPost.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostForm from '../components/PostForm';
+
+function EditPost() {
+  return (
+    <div>
+      <h1>Edit Post</h1>
+      <PostForm />
+    </div>
+  );
+}
+
+export default EditPost;
